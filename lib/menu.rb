a280426eb1a1010d94c618f7161e255f95e4e360
juliatan <hello@julia-tan.com>
2014-06-07 00:50:29 +0100

    Menu class creates an array, with dishes included as hashes

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,11 @@
 class Menu
 
+  def contents
+    @contents ||= []
+  end
 
+  def add(dish)
+    contents << { name: dish.name, price: dish.price }
+  end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,5 +2,15 @@ require 'menu'
 
 describe Menu do
 
+  let(:menu) { Menu.new }
 
+  it 'can create an array' do
+    expect(menu.contents).to eq []
+  end
+
+  it 'can add dishes' do
+    dish = double :dish, name: "Tomato Soup", price: "£1.50"
+    menu.add(dish)
+    expect(menu.contents).to eq [{name: "Tomato Soup", price: "£1.50"}]
+  end
 end
