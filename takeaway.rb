ada6dbc8a214c7a90a84b0e514cc3adfe28a7df1
juliatan <hello@julia-tan.com>
2014-06-07 00:58:32 +0100

    Added takeaway.rb file to run file in IRB

---
+++ takeaway.rb
@@ -0,0 +1,17 @@
+require './lib/client'
+require './lib/dish'
+require './lib/menu'
+
+@menu = Menu.new
+
+@dish1 = Dish.new("Tomato Soup", "£1.50")
+@dish2 = Dish.new("Vegetable Soup", "£2.50")
+@dish3 = Dish.new("Minestrone Soup", "£3.50")
+@dish4 = Dish.new("Beef Soup", "£4.50")
+@dish5 = Dish.new("Chicken Soup", "£5.50")
+
+@menu.add(@dish1)
+@menu.add(@dish2)
+@menu.add(@dish3)
+@menu.add(@dish4)
+@menu.add(@dish5)
