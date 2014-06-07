dca9336d5224704c4da7499ca3ea47c7b0662541
juliatan <hello@julia-tan.com>
2014-06-07 22:09:17 +0100

    Amended takeaway.rb (the run file) to initialise menu, order and ui automatically

--- takeaway.rb
+++ takeaway.rb
@@ -8,20 +8,21 @@ require './lib/menu'
 require './lib/order'
 require './lib/ui'
 
-@menu = Menu.new
+menu = Menu.new
 
-@dish1 = Dish.new("Tomato Soup", "£1.50")
-@dish2 = Dish.new("Vegetable Soup", "£2.50")
-@dish3 = Dish.new("Minestrone Soup", "£3.50")
-@dish4 = Dish.new("Beef Soup", "£4.50")
-@dish5 = Dish.new("Chicken Soup", "£5.50")
+dish1 = Dish.new("Tomato Soup", "£1.50")
+dish2 = Dish.new("Vegetable Soup", "£2.50")
+dish3 = Dish.new("Minestrone Soup", "£3.50")
+dish4 = Dish.new("Beef Soup", "£4.50")
+dish5 = Dish.new("Chicken Soup", "£5.50")
 
-@menu.add(@dish1)
-@menu.add(@dish2)
-@menu.add(@dish3)
-@menu.add(@dish4)
-@menu.add(@dish5)
+menu.add(dish1)
+menu.add(dish2)
+menu.add(dish3)
+menu.add(dish4)
+menu.add(dish5)
 
-@order = Order.new
+order = Order.new
+ui = UI.new
 
-@ui = UI.new
+ui.ordering_process(order, menu)
