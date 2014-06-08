de4c8686cd7eed3132f59577352b2fc55f6426e5
juliatan <hello@julia-tan.com>
2014-06-08 14:05:09 +0100

    Reinstalled Webmock

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -1,7 +1,12 @@
+# require_relative 'spec_helper'
 # require 'sms'
 
 # describe SMS do
+#   it 'should query Twilio API' do
+#     uri = URI('/2010-04-01/Accounts/AC632e1c5c07fe63b8ff585dda3ac085c6/Messages')
 
-#   it 'should'
+#     response = Net::HTTP.get(uri)
 
+#     expect(response).to be_an_instance_of(String)
+#   end
 # end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+# # http://robots.thoughtbot.com/how-to-stub-external-services-in-tests
+
+# require 'webmock/rspec'
+# WebMock.disable_net_connect!(allow_localhost: true)
